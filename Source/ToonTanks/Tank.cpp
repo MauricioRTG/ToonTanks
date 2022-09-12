5e967f2f760f3093daefea9559b236981458cb44
MauricioTenorio <xpigeonmt@gmail.com>
2022-09-11 21:17:39 -0500

    119. Creating Child C++ Classes (Part 2)

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+ATank::ATank()
+{
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camara"));
+	CameraComp->SetupAttachment(SpringArmComp);
+}
